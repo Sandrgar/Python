3853e99aa5a9f5d86a879f1db3eec3c119c2476c
Alessandro <al3ss4ndr@hotmail.it>
2023-11-21 19:07:02 +0100

    Def paridispari

--- primoprogramma.py
+++ primoprogramma.py
@@ -1,1 +1,12 @@
 print ("Corso Python")
+
+def paridispari ():
+    inp = input("inserisci un numero:")
+    numero = int(inp)
+    modulo = numero % 2
+    if modulo == 0:
+        print ("Il numero è Pari")
+    else: 
+        print ("Il numero è Dispari")
+
+paridispari()
